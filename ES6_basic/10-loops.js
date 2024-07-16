48b065c3275f7af31608a8420c43f891444f328e
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-16 11:27:14 +0100

    task10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disable-next-line
   const new_array = [];
   for (const value of array) {
     new_array.push(appendString + value);
   }
+  // eslint-disable-next-line
   return new_array;
 }
