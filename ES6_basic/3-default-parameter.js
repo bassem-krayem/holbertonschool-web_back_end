837a9f05310144fa8d1f61dab30fdf4b1e75b558
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-16 07:39:05 +0100

    task3

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,7 +1,6 @@
 export default function getSumOfHoods(
   initialNumber,
   expansion1989 = 89,
-  // eslint-disable-next-line
   expansion2019 = 19
 ) {
   return initialNumber + expansion1989 + expansion2019;
