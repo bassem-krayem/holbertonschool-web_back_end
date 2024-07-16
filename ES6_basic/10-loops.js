7efc4353f61db7fd98c1ef98835f3f94578215af
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-16 11:25:24 +0100

    task10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const new_array = [];
+  for (const value of array) {
+    new_array.push(appendString + value);
+  }
+  return new_array;
+}
