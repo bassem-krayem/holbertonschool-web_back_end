3d7b143281b650100c5f7e2388c141363c7d4d2b
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-17 09:18:13 +0100

    task4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+//eslint-disable-next-line
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof amount !== 'number' || Number.isNaN(amount)) {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!currency instanceof Currency) {
+      throw new TypeError('currency is not an instance of Currency class');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return conversionRate * amount;
+  }
+}
