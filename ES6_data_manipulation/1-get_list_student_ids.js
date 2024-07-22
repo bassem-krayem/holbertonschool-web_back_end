5c0c1663a274a9b5f7d7a94a63316d7ec3ed2aa5
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-22 09:12:06 +0100

    task1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,7 @@
 export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
+    // eslint-disable-next-line
   } else {
     return array.map((id) => id.id);
   }
