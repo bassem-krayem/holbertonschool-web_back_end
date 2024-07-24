4b43dd1dc3044fe250d27145d632914b659a17d1
bassem-krayem <bassem.krayem09@gmail.com>
2024-07-24 12:36:31 +0100

    task2: Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      resolve({ status: 200, body: 'success' });
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
-      reject(new Error());
+      return new Error();
     });
 }
