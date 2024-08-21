88e216280228915feedcd63670dc0661ee737bc3
bassem-krayem <bassem.krayem09@gmail.com>
2024-08-21 20:05:47 +0100

    task1: Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-This module defines a `Server` class used to paginate a dataset of popular 
-baby names. The dataset is loaded from a CSV file and cached for efficient 
+This module defines a `Server` class used to paginate a dataset of popular
+baby names. The dataset is loaded from a CSV file and cached for efficient
 pagination.
 
 Module Contents:
@@ -11,19 +11,20 @@ Module Contents:
     - `__dataset` (List[List]): A cached dataset read from the CSV file.
   - Methods:
     - `__init__()`: Initializes a new `Server` instance with an empty dataset.
-    - `dataset() -> List[List]`: Loads the dataset from the CSV file if not 
+    - `dataset() -> List[List]`: Loads the dataset from the CSV file if not
       already cached and returns it, excluding the header row.
-    - `get_page(page: int = 1, page_size: int = 10) -> List[List]`: Retrieves 
-      a list of rows for a specified page and page size. It raises an 
+    - `get_page(page: int = 1, page_size: int = 10) -> List[List]`: Retrieves
+      a list of rows for a specified page and page size. It raises an
       `AssertionError` if the page or page size are not positive integers.
-- `index_range(page: int, page_size: int) -> Tuple[int, int]`: A helper 
-  function that calculates the start and end indices for a specific page 
-  based on the given page number and page size. 
+- `index_range(page: int, page_size: int) -> Tuple[int, int]`: A helper
+  function that calculates the start and end indices for a specific page
+  based on the given page number and page size.
 
 Usage Example:
     server = Server()
     page_data = server.get_page(page=2, page_size=5)
-    print(page_data)  # Output will be the rows for page 2 with 5 items per page
+    print(page_data)
+    # Output will be the rows for page 2 with 5 items per page
 """
 
 import csv
