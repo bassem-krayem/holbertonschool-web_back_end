51c526712bc7e65a84b0436dd08b761190e8ab9f
bassem-krayem <bassem.krayem09@gmail.com>
2024-08-21 15:15:14 +0100

    tak1: Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,12 +9,12 @@ Functions:
 """
 
 import _asyncio
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Collects numbers from the async_generator using an async
     comprehension and returns them as a list.
