19a157d55215ab7e7e432342e522c95207a7271e
bassem-krayem <bassem.krayem09@gmail.com>
2024-08-28 17:18:23 +0100

    task11: find by topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""a function that returns the list of school having a specific topic:"""
+
 
 def schools_by_topic(mongo_collection, topic):
+    """a function that returns the list of school having a specific topic:"""
     return mongo_collection.find({"topics": topic})
+
+
+if __name__ == "__main__":
+    schools_by_topic(mongo_collection, topic)
