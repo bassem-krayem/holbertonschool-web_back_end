70e79575aebc9c99a4f0e462659699c21dcf3023
bassem-krayem <bassem.krayem09@gmail.com>
2024-08-28 18:12:46 +0100

    task12:  logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,11 +8,11 @@ col = client.logs.nginx
 
 print(f"{col.count_documents({})} logs")
 print("Methods:")
-print(f"method GET: {col.count_documents({'method': 'GET'})}")
-print(f"method POST: {col.count_documents({'method': 'POST'})}")
-print(f"method PUT: {col.count_documents({'method': 'PUT'})}")
-print(f"method PATCH: {col.count_documents({'method': 'PATCH'})}")
-print(f"method DELETE: {col.count_documents({'method': 'DELETE'})}")
+print(f"\tmethod GET: {col.count_documents({'method': 'GET'})}")
+print(f"\tmethod POST: {col.count_documents({'method': 'POST'})}")
+print(f"\tmethod PUT: {col.count_documents({'method': 'PUT'})}")
+print(f"\tmethod PATCH: {col.count_documents({'method': 'PATCH'})}")
+print(f"\tmethod DELETE: {col.count_documents({'method': 'DELETE'})}")
 print(
     f"{col.count_documents({'method': 'GET', 'path': '/status'})} "
     f"status check"
