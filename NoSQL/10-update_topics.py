ad1359fbcd82f1ab70921fc8412743d5d65ac0b2
bassem-krayem <bassem.krayem09@gmail.com>
2024-08-28 15:41:08 +0100

    task10: update documents

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def update_topics(mongo_collection, name, topics):
+    return mongo_collection.update_many({"name": name},
+                                        {"$set": {"topics": topics}})
+
+
+if __name__ == "__main__":
+    update_topics(mongo_collection, name, topics)
