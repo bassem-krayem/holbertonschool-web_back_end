8b53e95a8b674a2183ade1828232dd66ec0486c2
bassem-krayem <bassem.krayem09@gmail.com>
2024-09-19 12:52:41 +0100

    taskk2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    if (data) {
+      const rows = data.trim().split('\n');
+      const totalStudents = rows.length - 1;
+
+      console.log(`Number of students: ${totalStudents}`);
+
+      const studentsByField = {};
+
+      rows.slice(1).forEach((row) => {
+        const columns = row.split(',');
+        const firstName = columns[0];
+        const field = columns[columns.length - 1];
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(firstName);
+      });
+      for (const field in studentsByField) {
+        if (Object.hasOwnProperty.call(studentsByField, field)) {
+          const students = studentsByField[field];
+          console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+        }
+      }
+    } else {
+      throw new Error('Cannot load the database');
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = countStudents;
