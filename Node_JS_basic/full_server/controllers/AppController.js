b2f39b683ea4c8abc3866eb230ede8cd214485d5
bassem-krayem <bassem.krayem09@gmail.com>
2024-09-22 14:36:41 +0100

    task8

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,7 @@
-export default class AppController{
-  static getHomepage(req, res) {
-    res.status(200);
-    res.send('Hello Holberton School!');
+class AppController {
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
   }
 }
+
+export default AppController;
