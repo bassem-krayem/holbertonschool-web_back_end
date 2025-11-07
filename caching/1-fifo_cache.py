edfdd99ca029c81dd4f166ed5bbdbf6cbc571a37
bassem-krayem <bassem.krayem09@gmail.com>
2025-11-07 19:45:54 +0100

    task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -47,7 +47,7 @@ class FIFOCache(BaseCaching):
                 key: of the dict
                 item: value of the key
         """
-        if key or item is not None:
+        if key is not None and item is not None:
             valuecache = self.get(key)
             if valuecache is None:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
