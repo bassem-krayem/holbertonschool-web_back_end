bdbe8d6d57c856b76f3cf7fb52974af36f15af49
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-10 01:54:28 +0000

    feat: the files for the task 2 in the caching project

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+LIFO caching system
+This module defines a LIFOCache class that inherits from BaseCaching
+and implements simple put and get methods.
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class
+    Inherits from BaseCaching and implements put and get methods.
+    """
+
+    def __init__(self):
+        """ Initialize the LIFOCache instance. """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache.
+        Args:
+            key (str): The key under which the item is stored.
+            item (any): The item to store in the cache.
+        """
+        if key is not None and item is not None:
+            if (len(self.cache_data) >= BaseCaching.MAX_ITEMS
+                    and key not in self.cache_data):
+                key_to_delete = list(self.cache_data.keys())[-1]
+                del self.cache_data[key_to_delete]
+                print(f"DISCARD: {key_to_delete}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache by key.
+        Args:
+            key (str): The key of the item to retrieve.
+        Returns:
+            The item associated with the key, or None if the key is not found.
+        """
+        return self.cache_data.get(key)
