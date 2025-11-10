b2cafa7a487caf29b55f367a677b8cea6611fce6
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-10 15:55:22 +0000

    feat: the files for task 3 for the basic_authentication project

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,10 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """
+    Authentication class to manage API authentication.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determine if authentication is required for a given path.
