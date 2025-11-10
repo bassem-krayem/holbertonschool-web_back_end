0bc184acc2d18c2cae367633e381682b4130f7ba
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-10 00:54:00 +0000

    feat: the task 0 files for the caching project

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+BasicCache module
+Defines a BasicCache class that inherits from BaseCaching
+and implements simple put and get methods.
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class
+    Inherits from BaseCaching and implements put and get methods.
+    """
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache.
+        Args:
+            key (str): The key under which the item is stored.
+            item (any): The item to store in the cache.
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache by key.
+        Args:
+            key (str): The key of the item to retrieve.
+        Returns:
+            The item associated with the key, or None if the key is not found.
+        """
+        return self.cache_data.get(key)
