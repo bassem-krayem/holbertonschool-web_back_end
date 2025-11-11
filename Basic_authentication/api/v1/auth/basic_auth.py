452954b000bf4e8fc0a6d88f7e9d98adde8b2b43
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 08:25:09 +0000

    feat: the files for task 10 of the basic_authentication project

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """Basic Authentication module."""
 
 from api.v1.auth.auth import Auth
+from typing import TypeVar, Tuple, Union
+from models.user import User
 import base64
 
 
@@ -73,3 +75,30 @@ class BasicAuth(Auth):
             user, password = decoded_base64_authorization_header.split(':', 1)
             return user, password
         return None, None
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Retrieves a User object based on email and password.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+        Returns:
+            TypeVar('User'): The User object or None if not found.
+        """
+        if (
+            user_email is None or not isinstance(user_email, str) and
+            user_pwd is None or not isinstance(user_pwd, str)
+        ):
+            return None
+        user = User()
+        user.email = user_email
+        user.password = user_pwd
+
+        if User.search({'email': user_email}):
+            for u in User.search({'email': user_email}):
+                if u.is_valid_password(user_pwd):
+                    return u
+        return None
