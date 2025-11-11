6c12d7af45291c7c33f1d33c8f935dd9b3c84a8c
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 06:51:47 +0000

    feat: the files for task 7 of the basic_authentication project

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,20 @@ class BasicAuth(Auth):
     """
     BasicAuth class for handling basic authentication.
     """
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        Extracts the Base64 part of the Authorization header.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the header or None if invalid.
+        """
+        if (authorization_header is not None
+                and isinstance(authorization_header, str)):
+            if authorization_header.startswith("Basic "):
+                return authorization_header.split(" ")[1]
+        return None
