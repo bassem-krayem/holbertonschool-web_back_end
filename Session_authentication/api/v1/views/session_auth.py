371a45082f1b489a0e7afac63b250ae2694a44e6
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 16:14:03 +0000

    feat: files for task 8 of the project session_authentication

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -21,17 +21,17 @@ def login() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     from models.user import User
 
     users = User.search({'email': email})
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
