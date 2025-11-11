d498d3d011a2a848ba00f6c0bd937e323f2944d4
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 07:50:59 +0000

    feat: the files for task 9 of the basic_authentication project

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,3 +52,15 @@ class BasicAuth(Auth):
             except Exception:
                 return None
         return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        if (
+            decoded_base64_authorization_header is not None and
+            isinstance(decoded_base64_authorization_header, str) and
+            ':' in decoded_base64_authorization_header
+        ):
+            user, password = decoded_base64_authorization_header.split(':', 1)
+            return user, password
+        return None, None
