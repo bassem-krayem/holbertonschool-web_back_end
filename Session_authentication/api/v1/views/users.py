958dec7f54ef5e5f210d024804e21cdfff89d06a
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 11:14:33 +0000

    feat: files for task 0 of the project session_authentication

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,15 +25,12 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if (
-        user_id is None and
-        request.current_user is None and
-        user_id != 'me'
-    ):
-        abort(404)
     if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
         user = request.current_user
-    user = User.get(user_id)
+    else:
+        user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
