4e9ddbc2c03e8cf3a6e26d1e0823ed15328f493e
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 12:42:52 +0000

    feat: the files for task 2 from the project session_authentication

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,14 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id
+        Args:
+            user_id (str): The user ID
+        Returns:
+            str: The session ID
+            or None if user_id is None or not a string
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
