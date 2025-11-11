d6c82154685c0b15a6f7648fe6e68a0a009347ce
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-11 12:49:23 +0000

    feat: the files for task3 from the project session_authentication

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,19 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Get a user_id based on a session_id
+        Args:
+            session_id (str): The session ID
+        Returns:
+            str: The user ID
+            or None if session_id is None or not found
+        """
+        if (
+            session_id is None and
+            not isinstance(session_id, str)
+        ):
+            return None
+        return self.user_id_by_session_id.get(session_id)
