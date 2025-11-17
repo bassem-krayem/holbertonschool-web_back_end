1142208cd0f791c934bad637d07d7660702b4ae7
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-17 09:03:11 +0000

    feat: task 1 for the user_authentication service project

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+"""DB module"""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -16,7 +14,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -36,7 +34,7 @@ class DB:
         hashed_password: str
     ) -> User:
         """
-            Add a new user to the database
+        Add a new user to the database
         Args:
             email (str): The user's email
             hashed_password (str): The user's hashed password
@@ -47,3 +45,22 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by given attributes
+        Args:
+            **kwargs: Arbitrary keyword arguments representing user attributes
+        Returns:
+            User: The User object that matches the criteria
+        Raises:
+            NoResultFound: If no user is found with the given attributes
+            InvalidRequestError: If an invalid attribute is provided
+        """
+        query = self._session.query(User)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError()
+            query = query.filter(getattr(User, key) == value)
+        user = query.one()
+        return user
