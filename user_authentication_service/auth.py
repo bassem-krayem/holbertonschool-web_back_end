24622951cbad8df83c7eb3f70b530006b865a5e0
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-17 10:58:38 +0000

    feat: task 9 of  the user_authentication_service project

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -56,3 +57,10 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Generate a new UUID.
+        Returns:
+            str: A new UUID string
+        """
+        return str(uuid.uuid4())
