d4629c7283d92902450c06c4ecd2bb8349e1dc36
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-17 13:14:58 +0000

    feat: task 15 from the user_authentication_service project

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -51,5 +51,17 @@ def logout():
     return redirect(url_for('home'))
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """
+    Profile route
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
