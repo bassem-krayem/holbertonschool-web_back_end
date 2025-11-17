e5c17c2b6ac3990c3d4ce62cface0f52cdc9b108
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-17 13:12:17 +0000

    feat: task 14 from the user_authentication_service project

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ main module of the flask app """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth
 
 AUTH = Auth()
@@ -36,6 +36,19 @@ def login():
     res = jsonify({"email": email, "message": "logged in"})
     res.set_cookie("session_id", session_id)
     return res
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """
+    Logout route
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
