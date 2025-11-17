ff7ce1919cc985b8a106af4c8959b1b471f1e5e0
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-17 09:50:50 +0000

    feat: task 5 from the user_authentication_service project

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """auth module"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,29 @@ def _hash_password(password: str) -> bytes:
         bytes: The hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+        Args:
+            email (str): The user's email
+            password (str): The user's plain text password
+        Returns:
+            User: The newly created User object
+        Raises:
+            ValueError: If a user with the given email already exists
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError(f"User {email} already exists")
+        hashed_password = _hash_password(password)
+        return self._db.add_user(email, hashed_password)
