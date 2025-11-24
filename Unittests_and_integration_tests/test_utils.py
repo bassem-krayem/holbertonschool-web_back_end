63d9e1aa522d2f9faadb0be8a6d1a431d36a8c00
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-24 13:01:29 +0000

    feat: task 0 of the unittest_and_integration_testing project

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+a module for testing utils functions
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    TestAccessNestedMap class to test access_nested_map function
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        expected: Any
+    ) -> None:
+        """
+        Test access_nested_map function with different inputs
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+unittest.main()
