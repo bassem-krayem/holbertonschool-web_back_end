9d57689fe89c62eaed59437edc78022a3124b5a0
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-24 13:31:28 +0000

    feat: task1 of the project unittest_and_integration_tests

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -18,7 +18,6 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     TestAccessNestedMap class to test access_nested_map function
     """
-    pass
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
