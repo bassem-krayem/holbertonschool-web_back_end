a247a35fec9fe74df6b9d1ec5d332b7603adf8ce
bassem <104699079+bassem-krayem@users.noreply.github.com>
2025-11-27 03:01:27 +0000

    feat: task3 from the project i18n

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -33,7 +33,7 @@ babel.init_app(app, locale_selector=get_locale)
 @app.route('/')
 def home():
     """Render the home page."""
-    return render_template('0-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
