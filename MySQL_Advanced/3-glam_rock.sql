2704ed29cbface3ab09b8b379dc6874d1c5b2258
bassem-krayem <bassem.krayem09@gmail.com>
2025-12-02 15:13:08 +0100

    feat: task 3 from the project mysql-advanced

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Query to find the lifespan of Glam rock bands in the metal_bands table.
 
 SELECT band_name,
-       COALESCE(split, YEAR(CURDATE())) - formed AS lifespan
+       COALESCE(split, 2024) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE 'Glam rock%'
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC, band_name ASC;
