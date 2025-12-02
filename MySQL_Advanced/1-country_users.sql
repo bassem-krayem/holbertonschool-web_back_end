2d97ba9558e875c27f2278f052f368e44d3395d7
bassem-krayem <bassem.krayem09@gmail.com>
2025-12-02 12:22:45 +0100

    feat: task1 from the project my-sql-advanced

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script to create a 'users' table with country information
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
