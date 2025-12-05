efbe95b0f57caff9a48efc6df3ec77755b5db784
bassem-krayem <bassem.krayem09@gmail.com>
2025-12-05 11:22:36 +0100

    feat: task0 from the project redis_basic

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: bytes) -> str:
+    def store(self, data: str | bytes | int | float) -> str:
         """Store the input data in Redis and return the generated key."""
         key = str(uuid4())
         self._redis.set(key, data)
