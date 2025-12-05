c6555a52dd320b5854793b08fd0ec1b3eec98e4b
bassem-krayem <bassem.krayem09@gmail.com>
2025-12-05 11:19:53 +0100

    feat: task0 from the project redis_basic

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module defines a Cache class that interacts with a Redis database to
+store data."""
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """Cache class for storing data in Redis."""
+    def __init__(self) -> None:
+        """Initialize the Cache with a Redis client and flush the database."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """Store the input data in Redis and return the generated key."""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
