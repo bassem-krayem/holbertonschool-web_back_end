06fa8e851d8826ef8501dd12f320599a1fd4a37b
bassem-krayem <bassem.krayem09@gmail.com>
2025-12-15 17:23:56 +0100

    feat: task 3 from the project unittests in js

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('uses Utils.calculateNumber with correct arguments', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore();
+  });
+});
